fed6430f075da47ff123091f5be87bceaad02408
SharkErik20 Erik <revaierik100@gmail.com>
2024-11-14 10:20:18 +0100

    Add project files.

---
+++ Allampolgar.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BevolkerungGUI
+{
+    internal class Allampolgar
+    {
+        public int Id { get; set; }
+        public string Nem { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+        public bool Dohanyzik { get; set; }
+        public string DohanyzoString => Dohanyzik ? "igen" : "nem";
+        public string Nemzetiseg { get; set; }
+        public string Nepcsoport { get; set; }
+        public string Tartomany { get; set; }
+        public int NettoJovedelem { get; set; }
+        public string IskolaiVegzettseg { get; set; }
+        public string PolitikaiNezet { get; set; }
+        public bool AktivSzavazo { get; set; }
+        public string AktivSzavazoString => AktivSzavazo ? "igen" : "nem";
+        public int SorFogyasztasEvente { get; set; }
+        public string SorString { get; set; }
+        public int KrumpliFogyasztasEvente { get; set; }
+        public string KrumpliString { get; set; }
+        public int HaviNettoBevetele => NettoJovedelem / 12;
+        public int Eletkor => DateTime.Now.Year - SzuletesiEv;
+
+        public Allampolgar(string row)
+        {
+            string[] adatok = row.Split(';');
+            Id = int.Parse(adatok[0]);
+            Nem = adatok[1];
+            SzuletesiEv = int.Parse(adatok[2]);
+            Suly = int.Parse(adatok[3]);
+            Magassag = int.Parse(adatok[4]);
+            Dohanyzik = adatok[5] == "igen";
+            Nemzetiseg = adatok[6];
+            if (Nemzetiseg == "német")
+            {
+                Nepcsoport = adatok[7];
+            }
+            else
+            {
+                Nepcsoport = null;
+            }
+            Tartomany = adatok[8];
+            NettoJovedelem = int.Parse(adatok[9]);
+            IskolaiVegzettseg = adatok[10];
+            PolitikaiNezet = adatok[11];
+            AktivSzavazo = adatok[12] == "igen";
+            SorString = adatok[13];
+            if (adatok[13] != "NA")
+            {
+                SorFogyasztasEvente = int.Parse(adatok[13]);
+            }
+            KrumpliString = adatok[14];
+            if (adatok[14] != "NA")
+            {
+                KrumpliFogyasztasEvente = int.Parse(adatok[14]);
+            }
+        }
+        public override string ToString()
+        {
+           return $"{Id} {Nem} {SzuletesiEv} {Suly}kg {Magassag}cm {(Dohanyzik? "dohányzik" : "nem dohányzik")} {Nemzetiseg} {Nepcsoport} {NettoJovedelem}EUR {IskolaiVegzettseg} {PolitikaiNezet} {(AktivSzavazo? "aktív szavazó" : "nem szavaz")} {SorFogyasztasEvente}l sör {KrumpliFogyasztasEvente}kg krumpli";
+        }
+
+        public string ToString(bool five) 
+        {
+            if (five)
+            {
+                return $"{Id}\t{Nem}\t{SzuletesiEv}\t{Suly}kg\t{Magassag}cm";
+            }
+            else
+            {
+                return $"{Id}\t{Nemzetiseg}\t{Nepcsoport}\t{Tartomany}\t{NettoJovedelem}€";
+            }
+        }
+
+    }
+   
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,276 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BevolkerungGUI
+{
+    
+public partial class MainWindow : Window
+    {
+        List<Allampolgar> allampolgar = new List<Allampolgar>();
+        Binding mybinding = new Binding();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (StreamReader be = new StreamReader(path: @"../../../src/bevölkerung.txt", encoding: UTF8Encoding.UTF8))
+            {
+                _ = be.ReadLine();
+                while (!be.EndOfStream)
+                {
+                    allampolgar.Add(new Allampolgar(be.ReadLine()));
+                }
+            }
+
+            for (int i = 1; i <= 40; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = i;
+                Feladatok.Items.Add(item);
+            }
+
+            Feladatok.SelectionChanged += Feladatok_SelectionChanged;
+
+           DataContext = allampolgar;
+        }
+
+        private void Feladatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MegoldasMondatos.Content = string.Empty;
+            MegoldasLista.Items.Clear();
+            MegoldasTeljes.ItemsSource = null;
+
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            string selectedValue = selectedItem.Content.ToString();
+
+            string methodName = $"Feladat{selectedValue}";
+            var method = typeof(MainWindow).GetMethod(methodName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            method?.Invoke(this, null);
+        }
+        #region Feladatok
+        private void Feladat1()
+        {
+            int maxNettoJovedelem = allampolgar.Max(polgar => polgar.NettoJovedelem);
+            MegoldasMondatos.Content = $"A legmagasabb nettó éves jövedelem: {maxNettoJovedelem} €";
+        }
+
+
+        private void Feladat2()
+        {
+            double atlagJovedelem = allampolgar.Average(polgar => polgar.NettoJovedelem);
+            MegoldasMondatos.Content = $"Az állampolgárok átlagos nettó éves jövedelme: {atlagJovedelem:0.00} €";
+        }
+
+        private void Feladat3()
+        {
+            var groupedByTartomany = allampolgar.GroupBy(polgar => polgar.Tartomany);
+
+            StringBuilder result = new StringBuilder();
+            foreach (var group in groupedByTartomany)
+            {
+                string tartomany = group.Key;
+                int count = group.Count();
+                result.AppendLine($"Tartomány: {tartomany}, Állampolgárok száma: {count}");
+            }
+
+            MegoldasLista.Items.Clear();
+            MegoldasLista.ItemsSource = result.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void Feladat4()
+        {
+            MegoldasTeljes.ItemsSource = allampolgar.Where(polgar => polgar.Nemzetiseg == "angolai");
+        }
+
+        private void Feladat5()
+        {
+            MegoldasTeljes.ItemsSource = allampolgar.Where(polgar => polgar.SzuletesiEv == allampolgar.Min(p => p.SzuletesiEv));
+        }
+
+        private void Feladat6()
+        {
+            var nonSmokers = allampolgar.Where(polgar => !polgar.Dohanyzik).ToList();
+            MegoldasLista.ItemsSource = nonSmokers.Select(polgar => $"Nem dohányzó állampolgár - Id: {polgar.Id}, Havi jövedelem: {polgar.HaviNettoBevetele} €").ToList();
+        }
+
+        private void Feladat7()
+        {
+
+        }
+
+        private void Feladat8()
+        {
+
+        }
+
+        private void Feladat9()
+        {
+
+        }
+
+        private void Feladat10()
+        {
+
+        }
+
+        private void Feladat11()
+        {
+
+        }
+
+        private void Feladat12()
+        {
+
+        }
+
+        private void Feladat13()
+        {
+
+        }
+
+        private void Feladat14()
+        {
+
+        }
+
+        private void Feladat15()
+        {
+
+        }
+
+        private void Feladat16()
+        {
+
+        }
+
+        private void Feladat17()
+        {
+
+        }
+
+        private void Feladat18()
+        {
+
+        }
+
+        private void Feladat19()
+        {
+
+        }
+
+        private void Feladat20()
+        {
+
+        }
+
+        private void Feladat21()
+        {
+
+        }
+
+        private void Feladat22()
+        {
+
+        }
+
+        private void Feladat23()
+        {
+
+        }
+
+        private void Feladat24()
+        {
+
+        }
+
+        private void Feladat25()
+        {
+
+        }
+
+        private void Feladat26()
+        {
+
+        }
+
+        private void Feladat27()
+        {
+
+        }
+
+        private void Feladat28()
+        {
+
+        }
+
+        private void Feladat29()
+        {
+
+        }
+
+        private void Feladat30()
+        {
+
+        }
+
+        private void Feladat31()
+        {
+
+        }
+
+        private void Feladat32()
+        {
+
+        }
+
+        private void Feladat33()
+        {
+
+        }
+
+        private void Feladat34()
+        {
+
+        }
+
+        private void Feladat35()
+        {
+
+        }
+
+        private void Feladat36()
+        {
+
+        }
+
+        private void Feladat37()
+        {
+
+        }
+
+        private void Feladat38()
+        {
+
+        }
+
+        private void Feladat39()
+        {
+
+        }
+
+        private void Feladat40()
+        {
+
+        }
+        #endregion
+    }
+}
